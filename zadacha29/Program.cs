d4839d75b19c143b934532f80efa9b21b2a7a58b
Sergey Korchagin <brodaga_777@mail.ru>
2022-08-05 10:18:31 +0400

    add zadacha29

--- zadacha29/Program.cs
+++ zadacha29/Program.cs
@@ -4,3 +4,32 @@
 // 6, 1, 33 -> [6, 1, 33]
 
 Console.Clear();
+
+int[] GetRandomArray(int arrayLenght)
+{
+	Random rnd = new Random();
+	int[] arr = new int[arrayLenght];
+
+	for(int i = 0; i < arr.Length; i++)
+	{
+		arr[i] = rnd.Next(0,1000);
+	}
+	return arr;
+}
+
+int[] array = GetRandomArray(8);
+WriteArrayToConsole(array);
+
+
+void WriteArrayToConsole(int[] array)
+{
+	Console.Clear();
+	Console.Write("[");
+	for(int i = 0; i < array.Length; i++)
+	{
+		Console.Write(array[i]);
+		if (i != array.Length-1)
+		Console.Write(",");
+	}
+	Console.WriteLine("]");
+}
